95cfbbfa24fedeade4c1a037796815c06144a238
hasanhayat <aylskhsmvz.v@gmail.com>
2024-11-14 23:07:30 +0500

    complete

--- script.js
+++ script.js
@@ -1,21 +1,67 @@
 let h1 = document.querySelector("h1");
+let startBtn = document.getElementById("startBtn");
+let stopBtn = document.getElementById("stopBtn");
+let resetBtn = document.getElementById("resetBtn");
 
-let minutes = "00";
-let hours = "00";
-let seconds = "00";
-let milliSecond = "00";
+let minutes = 0;
+let hours = 0;
+let seconds = 0;
+let intervalId = null;
 
-setInterval(()=>{
-    
-    let watch = `${hours}:${minutes}:${seconds++}`;
-    if(seconds >= 60){
+function updateWatch() {
+  let formattedHours, formattedMinutes, formattedSeconds;
+
+  if (hours < 10) {
+    formattedHours = "0" + hours;
+  } else {
+    formattedHours = hours;
+  }
+
+  if (minutes < 10) {
+    formattedMinutes = "0" + minutes;
+  } else {
+    formattedMinutes = minutes;
+  }
+
+  if (seconds < 10) {
+    formattedSeconds = "0" + seconds;
+  } else {
+    formattedSeconds = seconds;
+  }
+
+  h1.innerText =
+    formattedHours + ":" + formattedMinutes + ":" + formattedSeconds;
+}
+
+startBtn.addEventListener("click", () => {
+  if (!intervalId) {
+    intervalId = setInterval(() => {
+      seconds++;
+      if (seconds >= 60) {
         seconds = 0;
-        minutes++
-    }
-    if(minutes >= 60){
+        minutes++;
+      }
+      if (minutes >= 60) {
         minutes = 0;
-        hours++
-    }
-    h1.innerText = watch;
-  
-},1000)
+        hours++;
+      }
+      updateWatch();
+    }, 1000);
+  }
+});
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(intervalId);
+  intervalId = null;
+});
+
+resetBtn.addEventListener("click", () => {
+  clearInterval(intervalId);
+  intervalId = null;
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
+  updateWatch();
+});
+
+updateWatch();
